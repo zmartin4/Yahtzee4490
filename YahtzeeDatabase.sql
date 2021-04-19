e8ab2fea43c4a1f1f34ffd71da503a64944c040e
Jacob Mayo <60887746+JacobMayo076@users.noreply.github.com>
2021-04-18 20:48:33 -0500

    Jacob - Formatting Change
    Something was not spaced out correctly and really bothered me, so I fixed it.

--- YahtzeeDatabase.sql
+++ YahtzeeDatabase.sql
@@ -1,10 +1,10 @@
 DROP TABLE The_User CASCADE CONSTRAINTS;
 
 CREATE TABLE The_User (
-	username			VARCHAR(20),
+	username		VARCHAR(20),
 	user_password		VARCHAR(16) CONSTRAINT theuser_userpassword_nn NOT NULL,
 	user_highscore		NUMERIC(4)) CONSTRAINT theuser_userhighscore_nn NOT NULL
 	CONSTRAINT theuser_username_pk PRIMARY KEY (username));
 
 /*Test Value*/
-INSERT INTO The_User VALUES('jmayo2@cub.uca.edu', 'he110W0r1d', 0);
+INSERT INTO The_User VALUES('jmayo2', 'he110W0r1d', 0);
